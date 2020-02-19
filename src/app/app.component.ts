ad252c3deff532e39ae369fac5dd4898c699d1e6
emu <bsse0927@iit.du.ac.bd>
2020-02-19 11:03:18 +0600

    upto property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularComponentInteraction';
+  Pagetitle = 'Angular Component Interaction';
+  imgUrl = 'https://picsum.photos/200';
 }
