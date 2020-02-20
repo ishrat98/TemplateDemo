f541477802013140d3316481887644f80340ed23
emu <bsse0927@iit.du.ac.bd>
2020-02-20 12:53:05 +0600

    upto ngOnChanges()

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,32 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
-  private LoggedIn: boolean;
+export class ChildComponent implements OnChanges {
+  @Input() loggedIn: boolean;
   message: string;
 
- get loggedIn(): boolean {
-    return this.LoggedIn;
-  }
+//  get loggedIn(): boolean {
+//     return this.LoggedIn;
+//   }
 
-  @Input()
-  set loggedIn(value: boolean) {
-    this.LoggedIn = value;
-    if (value === true) {
-      this.message = 'Welcome back Emu!';
-    } else {
-      this.message = 'Please log in';
-    }
-  }
+//   @Input()
+//   set loggedIn(value: boolean) {
+//     this.LoggedIn = value;
+//     if (value === true) {
+//       this.message = 'Welcome back Emu!';
+//     } else {
+//       this.message = 'Please log in';
+//     }
+//   }
 
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges( changes: SimpleChanges) {
+    console.log(changes);
+    const loggedInValue = changes.loggedIn;
+    if (loggedInValue.currentValue === true) {
+      this.message = 'Welcome back Emu!!';
+    } else {
+      this.message = 'Please log in';
+    }
   }
 
 }
